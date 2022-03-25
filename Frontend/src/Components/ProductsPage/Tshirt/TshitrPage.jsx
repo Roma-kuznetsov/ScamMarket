a358c824ef1b534b2d54d7bcfb6451d96f133552
Roma-kuznetsov <Roma_kuznecov_2000@mail.ru>
2022-03-25 15:08:41 +0300

    path /favorits and favoritsContainer

--- Frontend/src/Components/ProductsPage/Tshirt/TshitrPage.jsx
+++ Frontend/src/Components/ProductsPage/Tshirt/TshitrPage.jsx
@@ -3,15 +3,22 @@ import InBar from "../InBar";
 import Paginator from "../Paginator";
 import ProductsList from "../ProductsList";
 import Sponsors from "../Sponsors";
+import FavoritsNull from "../../ButtonsFrarment/FavoritsNull"
 
 const TshitrPage = (props) => {
     const pageCount = Math.ceil(props.totalItemsCount / props.pageSize)
     return (
         <>
             <InBar />
-            <Sponsors />
-            <ProductsList idUser={props.idUser} addFavThunk={props.addFavThunk} favorites={props.favorites} body={props.body} setItem={props.setItem} selectedItem={props.selectedItem} select={props.select} />
-            <Paginator onPageChanged={props.onPageChanged} pageCount={pageCount} currentPage={props.currentPage} setCurrentPage={props.setCurrentPage} />
+            <Sponsors /> 
+            <ProductsList idUser={props.idUser} addFavThunk={props.addFavThunk} 
+            favorites={props.favorites} body={props.body} setItem={props.setItem} 
+            selectedItem={props.selectedItem} select={props.select} />
+            
+            {pageCount > 1? 
+            <Paginator onPageChanged={props.onPageChanged} pageCount={pageCount} 
+            currentPage={props.currentPage} setCurrentPage={props.setCurrentPage} />
+            : null}
         </>
     )
 }
